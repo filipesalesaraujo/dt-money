d652660d526b3ba49bd10aef7a73c8fdf255083e
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-08-27 14:16:29 -0300

    Estrutura visual - Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import {ThemeProvider} from "styled-components";
 import {defaultTheme} from "./styles/themes/default";
 import {GlobalStyle} from "./styles/global";
 
+import {Header} from "./components/Header";
+
 export function App() {
     return (
         <ThemeProvider theme={defaultTheme}>
-            <GlobalStyle />
-            <h1>ola</h1>
+            <GlobalStyle/>
+            <Header/>
         </ThemeProvider>
     )
 }
